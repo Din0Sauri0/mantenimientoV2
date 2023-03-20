d08235048f286b2167eb384f545fc2f26f9e4a33
Gustavo Ovalle <gustavo.ovalle.zambrano@gmail.com>
2023-03-19 23:07:06 -0300

    backdrop deleting

--- resources/js/delete_backdrop.js
+++ resources/js/delete_backdrop.js
@@ -1,12 +1,13 @@
 const btn = document.querySelector('#cancel_btn');
+
 const delete_backdrop = () => {
-    console.log('click');
     let index = document.querySelectorAll('div[modal-backdrop]');
-    // index.setAttribute('id', 'backdrop');
-    console.log(index);
-    index.forEach(element => {
-        element.remove();
-    });
+    if(index.length > 1){
+        index[1].remove();
+    }else{
+        return;
+    }
 }
+
 btn.addEventListener('click', delete_backdrop);
 
